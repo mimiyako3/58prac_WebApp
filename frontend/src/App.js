2bf58a64a83f54e2d6fcdf281c972a4c7fa9a766
Koseeee-27 <kosekose2741@gmail.com>
2025-04-19 19:09:21 +0900

    ルーティング作成

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import AllTodo from './todoList/AllTodo';
+import Home from './Home';
+import Timeline from './timeline/Timeline'
+import Start from './Start';
+
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Timeline />}/>
+          <Route path="/todo" element={<AllTodo />}/>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/start" element={<Start />}/>
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
