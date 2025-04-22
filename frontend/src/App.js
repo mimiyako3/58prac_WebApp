f16a61790a32fcd00f5c595dc6c8e9f86952302f
Koseeee-27 <kosekose2741@gmail.com>
2025-04-22 15:19:31 +0900

    ヘッダー追加してそれぞれのページのルーティング実装

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,14 @@ import AllTodo from './todoList/AllTodo';
 import Home from './Home';
 import Timeline from './timeline/Timeline'
 import Start from './Start';
+import Header from './hamburger/Header';
 
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Timeline />}/>
           <Route path="/todo" element={<AllTodo />}/>
--- frontend/src/hamburger/Header.js
+++ frontend/src/hamburger/Header.js
@@ -1,14 +1,31 @@
 /**
  * ハンバーガーメニュー(ボタンとサイドバーをまとめて)を表示するクラス
- * @author 近代花子
+ * @author kosei
  */
 
 
 
 import React from 'react'
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 export default function Header() {
   return (
-    <div>Header</div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">Todo×SNS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href='/'  >タイムライン</Nav.Link>
+            <Nav.Link href='/todo' >todo+投稿</Nav.Link> 
+            <Nav.Link ahref='/home' >自分の投稿</Nav.Link> 
+            <Nav.Link href='/start' >スタート</Nav.Link> 
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
