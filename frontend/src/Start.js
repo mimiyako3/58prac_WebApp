9c84aed096fb333097e6813063edee5672a146d9
wakana <2312110232@kindai.ac.jp>
2025-05-05 23:52:27 +0900

    ボタンと説明のcss追加

--- frontend/src/Start.js
+++ frontend/src/Start.js
@@ -1,6 +1,6 @@
 /**
  * スタート画面の作成
- * @author 花子
+ * @author 若菜
  */
 
 import React from 'react'
@@ -9,10 +9,15 @@ import './Start.css';
 export default function Start() {
   return (
     <div className="start-container">
-      {/* タイトル追加 */}
-      <h1 className="title">Todo×SNS</h1>
-      <button className="btn">ログイン</button>
-      <button className="btn">新規登録</button>
+        {/* タイトル追加 */}
+        <h1 className="title">Todo×SNS</h1>
+      <div className="button-container">
+        <button className="btn">ログイン</button>
+        <button className="btn">新規登録</button>
+      </div>
+       <p className="text">
+        ここに説明書くよ
+       </p>
     </div>
     
 
