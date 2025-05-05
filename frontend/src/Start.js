33933096a31cff9f54468b4e232255928abd2794
wakana <2312110232@kindai.ac.jp>
2025-05-05 23:12:29 +0900

    タイトルのcss追加

--- frontend/src/Start.js
+++ frontend/src/Start.js
@@ -4,9 +4,17 @@
  */
 
 import React from 'react'
+import './Start.css';
 
 export default function Start() {
   return (
-    <div>Start</div>
+    <div className="start-container">
+      {/* タイトル追加 */}
+      <h1 className="title">Todo×SNS</h1>
+      <button className="btn">ログイン</button>
+      <button className="btn">新規登録</button>
+    </div>
+    
+
   )
 }
